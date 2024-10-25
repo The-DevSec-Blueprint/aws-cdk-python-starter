59a991e40c763acc4b86ac4391dcb29a4671eb71
Damien Burks <damien@damienjburks.com>
2024-10-24 20:23:14 -0500

    updating

--- tests/unit/test_test_stack.py
+++ tests/unit/test_test_stack.py
@@ -1,4 +1,6 @@
-import aws_cdk as core
+import os
+
+import aws_cdk as cdk
 import aws_cdk.assertions as assertions
 
 from test.test_stack import TestStack
@@ -6,8 +8,14 @@ from test.test_stack import TestStack
 # example tests. To run these tests, uncomment this file along with the example
 # resource in test/test_stack.py
 def test_sqs_queue_created():
-    app = core.App()
-    stack = TestStack(app, "test")
+    # Use the environment variables if available, otherwise use the default account/region
+    env = cdk.Environment(
+        account=os.environ.get("CDK_DEFAULT_ACCOUNT"),
+        region=os.environ.get("CDK_DEFAULT_REGION")
+    )
+
+    app = cdk.App()
+    stack = TestStack(app, "test", env=env)
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
